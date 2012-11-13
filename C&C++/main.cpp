0e213ff5fcbd297c33ef24d9d28f43f47b4508b0
leego <leegoex@gmail.com>
2012-11-13 11:00:57 +0800

    First branch

--- C&C++/main.cpp
+++ C&C++/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	cout<<"Hello World\n";
+	string str = "Hello World!\n";
+	cout<<str;
 	return 0;
 }
