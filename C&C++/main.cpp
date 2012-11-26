2440adf3f3d28eb6b6ee703f3a1a8a150d3d320b
root <root@leego.(none)>
2012-11-26 20:49:35 +0800

    Study to use boost

--- C&C++/main.cpp
+++ C&C++/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <string>
-using namespace std;
-
-int main(int argc, char *argv[])
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+using namespace boost::posix_time;
+int main(int argc, char *argv[]) 
 {
-	char prompt[] = "Hello World!\n";
-	cout<<prompt;
-	string str = "Hello World!\n";
-	cout<<str;
+	ptime now = second_clock::universal_time();
+	std::cout << "Current Time is: "<< to_iso_extended_string(now)<< std::endl;
+	ptime now_2 = microsec_clock::universal_time();
+	std::cout << "Current Time is: "<< to_iso_extended_string(now_2)<< std::endl;
+
 	return 0;
 }
